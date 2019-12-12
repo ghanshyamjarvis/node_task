3a46b9ce52c44b8240fc5617e46166f4631f62bd
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2019-12-12 18:55:26 +0530

    Add middle ware to check other links in main page

--- Flattern/index.js
+++ Flattern/index.js
@@ -26,6 +26,9 @@ var express = require ('express');
 var app = express();
 var path =require('path')
 
+
+app.use(express.static(path.join(__dirname)))
+
     app.get('/',function (request,response)
     {
       console.log(request,url)
