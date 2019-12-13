3ebc4afe8c34705f089b865ec127a069564536cf
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2019-12-13 11:33:52 +0530

    no chang

--- templated-hielo/index.js
+++ templated-hielo/index.js
@@ -1,3 +1,4 @@
+/*
 var express = require ('express')
 var app =express()
 var path = require('path')
@@ -6,11 +7,26 @@ app.use(express.static(path.join(__dirname)))
 
 app.get('./',function (req,res)
 {
-  console.log(req,url)
+  console.log(req,url);
   res.status(200).sendFile(__dirname,'index.js')
 });
 
 app.listen(4000,function ()
 {
   console.log("Connected Successful at Port 4000")
+});
+*/
+var express =require ('express');
+var app =express();
+var path =require('path');
+
+app.get('/',function (req,res)
+{
+  console.log(req,url)
+  res.status(200).sendFile(__dirname,'index.html')
+});
+
+app.listen(4000,function ()
+{
+console.log("4000 port connect")
 });
